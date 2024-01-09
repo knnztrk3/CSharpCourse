98a7ca082fa891edeb7549b7e41d22ebdf111915
knnztrk <kenan.ozturk3@hotmail.com>
2024-01-09 15:06:33 +0300

    Types and veriables, Conditionals

---
+++ Conditionals/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var number1 = 11;
+			var number2 = 10;
+			if (number1 == 10)
+			{
+				Console.WriteLine("Number1 is 10");
+			}
+			else if (number1 == 20)
+			{
+				Console.WriteLine("Number1 is 20");
+			}
+			else
+			{
+				Console.Write("Number1 is not 10 or 20");
+			}
+
+			//Single Line If
+			Console.WriteLine(number2 == 10 ? "Number2 is 10" : "Number2 is not 10");
+
+			//switch-start
+			switch (number1)
+			{
+				case 10:
+					Console.WriteLine("Number1 is 10");
+					break;
+				case 20:
+					Console.WriteLine("Number1 is 20");
+					break;
+				default:
+					Console.Write("Number1 is not 10 or 20");
+					break;
+			}//sw
+
+			//coklu-if
+			if (number1 >= 0 && number1 <= 100)
+			{
+				Console.WriteLine("Number1 is between 0-100");
+			}
+			else if (number1 > 100 && number1 <= 200)
+			{
+				Console.WriteLine("Number1 is between 101-200");
+			}
+			else if (number1 > 200 || number1 < 0)
+			{
+				Console.WriteLine("Number1 is less than 0 or greater than 200");
+			}
+
+			//ic ice if
+			if (number1 < 100)
+			{
+				if (number1 >= 90 && number1 < 95)
+				{
+
+				}
+				else
+				{
+
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ VeriTipleriTypesAndVeriables/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeriTipleriTypesAndVeriables
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Value Types
+			//Console.WriteLine("Hello World!");
+			
+			double number5 = 10.4;
+			decimal number6 = 10.4m;
+			char character = 'K';
+			bool condition = false;
+			byte number4 = 0; // 0 ila 255
+			short number3 = 32767;
+			int number1 = 2147483647;
+			long number2 = 9223372036854775807;
+			var number7 = 10;
+			number7 = 'A';
+
+			//if("Friday" == )
+			//if (Days.Friday == )
+
+			Console.WriteLine("Number1 is {0}", number1);
+			Console.WriteLine("Number2 is {0}", number2);
+			Console.WriteLine("Number3 is {0}", number3);
+			Console.WriteLine("Number4 is {0}", number4);
+			Console.WriteLine("Number5 is {0}", number5);
+			Console.WriteLine("Number6 is {0}", number6);
+			Console.WriteLine("Number7 is {0}", number7);
+			Console.WriteLine("Character is {0}", (int)character);
+			Console.WriteLine(Days.Friday); // Output: Friday
+			Console.WriteLine((int)Days.Friday); // Output: 4
+			Console.ReadLine();
+		}
+	}
+
+	enum Days
+	{
+		// Başlangıç değeri verebiliriz, 10,11,12,13,14,15,16
+		//Monday = 10, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+		Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+	}
+}
