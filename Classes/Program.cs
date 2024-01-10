3e70ad64662cc872ecf933e323cf834b9889ca3e
knnztrk <kenan.ozturk3@hotmail.com>
2024-01-10 22:34:37 +0300

    Classes-1

---
+++ Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CustomerManager customerManager = new CustomerManager();
+			customerManager.Add();
+			customerManager.Update();
+			customerManager.Delete();
+
+			Console.ReadLine();
+		}
+	}
+
+	class CustomerManager
+	{
+		public void Add()
+		{
+			Console.WriteLine("Customer Added!");
+		}
+
+		public void Update()
+		{
+			Console.WriteLine("Customer Updated!");
+		}
+
+		public void Delete()
+		{
+			Console.WriteLine("Customer Deleted!");
+		}
+	}
+}
