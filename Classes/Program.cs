ec49d9c0e5cd964c7b2a150e05979b2a824f525d
knnztrk <kenan.ozturk3@hotmail.com>
2024-01-10 22:38:23 +0300

    Classes-3

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -23,40 +23,4 @@ namespace Classes
 			Console.ReadLine();
 		}
 	}
-
-	class CustomerManager
-	{
-		public void Add()
-		{
-			Console.WriteLine("Customer Added!");
-		}
-
-		public void Update()
-		{
-			Console.WriteLine("Customer Updated!");
-		}
-
-		public void Delete()
-		{
-			Console.WriteLine("Customer Deleted!");
-		}
-	}
-
-	class ProductManager
-	{
-		public void Add()
-		{
-			Console.WriteLine("Product Added!");
-		}
-
-		public void Update()
-		{
-			Console.WriteLine("Product Updated!");
-		}
-
-		public void Delete()
-		{
-			Console.WriteLine("Product Deleted!");
-		}
-	}
 }
