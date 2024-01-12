f84de9537b545c13517936f157b8c886431290d3
knnztrk <kenan.ozturk3@hotmail.com>
2024-01-12 15:45:33 +0300

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//InterfacesIntro();
+			//Demo();
+
+			ICustomerDal[] customerDals = new ICustomerDal[3]
+			{
+				new SqlServerCustomerDal(),
+				new OracleCustomerDal(),
+				new MySqlCustomerDal(),
+			};
+
+			foreach (var customerDal in customerDals)
+			{
+				customerDal.Add();
+			}
+
+			Console.ReadLine();
+		}
+
+		private static void Demo()
+		{
+			CustomerManager customerManager = new CustomerManager();
+			customerManager.Add(new SqlServerCustomerDal());
+			customerManager.Add(new OracleCustomerDal()); // Veritabanımızı değiştirmek istediğimizde sadece implement etmemiz yeterli olucak 
+		}
+
+		private static void InterfacesIntro()
+		{
+			PersonManager manager = new PersonManager();
+			Customer customer = new Customer
+			{
+				Id = 1,
+				FirstName = "firstNamecustomer",
+				LastName = "lastName",
+				Address = "adres1",
+				Age = 15
+			};
+
+			Student student = new Student
+			{
+				Id = 1,
+				FirstName = "firstNamestudent",
+				LastName = "lastName",
+				Departmant = "department",
+				Age = 15
+			};
+
+			Worker worker = new Worker
+			{
+				Id = 1,
+				FirstName = "firstNameworker",
+				LastName = "lastName",
+				Departmant = "department",
+				Age = 15
+			};
+
+			//manager.Add(new Customer
+			//{
+			//	Id = 1,
+			//	FirstName = "firstName",
+			//	LastName = "lastName",
+			//	Address = "adres1",
+			//	Age = 15
+			//});
+
+			manager.Add(student);
+			manager.Add(customer);
+			manager.Add(worker);
+
+
+			//IPerson person = new IPerson(); // Soyut nesneler new'lenemez
+			IPerson person = new Customer();
+			person.FirstName = "TEST";
+			IPerson person2 = new Student();
+			IPerson person3 = new Worker();
+		}
+	}
+
+	interface IPerson
+	{
+		int Id { get; set; }
+		string FirstName { get; set; }
+		string LastName { get; set; }
+		int Age { get; set; }
+	}
+
+	class Customer : IPerson
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+
+		public string Address { get; set; }
+	}
+
+	class Student : IPerson
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+
+		public string Departmant { get; set; }
+	}
+
+	class Worker : IPerson
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+
+		public string Departmant { get; set; }
+	}
+
+	class PersonManager
+	{
+		//public void Add(Customer customer)
+		//{
+		//	Console.WriteLine(customer.FirstName);
+		//}
+
+		public void Add(IPerson person)
+		{
+			Console.WriteLine(person.FirstName);
+		}
+	}
+}
---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IWorker[] workers = new IWorker[3]
+			{
+				new Manager(),
+				new Worker(),
+				new Robot()
+			};
+
+			foreach (var worker in workers)
+			{
+				worker.Work();
+			}
+
+			IEat[] eats = new IEat[2]
+			{
+				new Manager(),
+				new Worker()
+			};
+
+			foreach (var eat in eats)
+			{
+				eat.Eat();
+			}
+			Console.ReadLine();
+		}
+	}
+
+	interface IWorker
+	{
+		void Work();
+	}
+
+	interface IEat
+	{
+		void Eat();
+	}
+
+	interface ISalary
+	{
+		void GetSalary();
+	}
+
+	class Manager : IWorker, IEat, ISalary
+	{
+		public void Eat()
+		{
+			Console.WriteLine("Manager Eats");
+		}
+
+		public void GetSalary()
+		{
+			Console.WriteLine("Manager GetSalaries");
+		}
+
+		public void Work()
+		{
+			Console.WriteLine("Manager Works");
+		}
+	}
+
+	class Worker : IWorker, ISalary, IEat
+	{
+		public void Eat()
+		{
+			Console.WriteLine("Worker Eats");
+		}
+
+		public void GetSalary()
+		{
+			Console.WriteLine("Worker GetSalaries");
+		}
+
+		public void Work()
+		{
+			Console.WriteLine("Worker Works");
+		}
+	}
+
+	class Robot : IWorker
+	{
+		public void Work()
+		{
+			Console.WriteLine("Robot Works");
+		}
+	}
+}
