9d4ac6e62c9cdbc900005291f14c4bef18fd4a6a
knnztrk <kenan.ozturk3@hotmail.com>
2024-01-13 17:39:40 +0300

    AbstractClasses, RecapDemo2

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Database oracleDatabase = new Oracle();
+			oracleDatabase.Add();
+			oracleDatabase.Delete();
+
+			Database sqlServerDatabase = new SqlServer();
+			sqlServerDatabase.Add();
+			sqlServerDatabase.Delete();
+
+			Console.ReadLine();
+		}
+	}
+
+	abstract class Database
+	{
+		public void Add()
+		{
+			Console.WriteLine("Added by default");
+		}
+
+		public abstract void Delete();
+	}
+
+	class SqlServer : Database
+	{
+		public override void Delete()
+		{
+			Console.WriteLine("Deleted by SqlServer");
+		}
+	}
+
+	class Oracle : Database
+	{
+		public override void Delete()
+		{
+			Console.WriteLine("Deleted by Oracle");
+		}
+	}
+}
