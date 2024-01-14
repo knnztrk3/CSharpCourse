d57e11b4d8855367bde22286f2a07ab719b41d90
knnztrk <kenan.ozturk3@hotmail.com>
2024-01-14 12:25:54 +0300

    Access Modifiers - Constructors

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+
+	class Customer
+	{
+		//private int ID { get; set; }
+		protected int ID { get; set; } 
+
+		public void Save()
+		{
+
+			int id; //default private
+			//ID
+		}
+
+		public void Delete()
+		{
+			//ID
+		}
+	}
+
+	class Student 
+	{
+		public void Save()
+		{
+			
+		}
+	}
+}
