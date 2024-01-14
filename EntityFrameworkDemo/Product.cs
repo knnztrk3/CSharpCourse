1fb6ef52fec82231027f10650213b0867bcdd5ed
knnztrk <kenan.ozturk3@hotmail.com>
2024-01-15 01:36:34 +0300

    Entity Framework Demo

---
+++ EntityFrameworkDemo/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+	public class Product 
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public decimal UnitPrice { get; set; }
+		public int StockAmount { get; set; }
+
+		public Product()
+		{
+
+		}
+
+		public Product(int ıd, string name, decimal unitPrice, int stockAmount)
+		{
+			Id = ıd;
+			Name = name;
+			UnitPrice = unitPrice;
+			StockAmount = stockAmount;
+		}
+	}
+}
